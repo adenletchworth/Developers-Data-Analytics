c4d6658cf79326974ab7ffb625e44b7a0feff229
adenletchworth <adenrletchworth@gmail.com>
2024-03-31 16:27:13 -0700

    feat: Push model for sync

--- NER/word2vec.py
+++ NER/word2vec.py
@@ -1,19 +1,23 @@
 import json
-from gensim.models import Word2Vec
 import re 
 
 file_path = './NER/NER_DATA/word2vec.txt'
+json_file_path = './NER/NER_DATA/word2vec.json'
 
 def preprocess_text(text):
     return re.sub(r'\W+', ' ', text.lower()).split()
 
-sentences = []  
+data = []
 
 with open(file_path, 'r', encoding='utf-8') as f:
     for line in f:
-        processed_line = preprocess_text(line)
-        sentences.append(processed_line)
+        original_line = line.strip() 
+        tokenized_line = preprocess_text(line)
+        if original_line:  
+            data.append({
+                "original": original_line,
+                "tokenized": tokenized_line
+            })
 
-model = Word2Vec(sentences, vector_size=100, window=5, min_count=1, workers=4)
-
-model.save("word2vec.model")
+with open(json_file_path, 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=4)
